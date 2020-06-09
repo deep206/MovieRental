655b33ef80313d4320daeb4261133a81720983ec
deep206 <deeppatel.ce@gmail.com>
2020-06-09 18:59:53 -0400

    Enable migrations and minor changes

---
+++ MovieRental/Models/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace MovieRental.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Movie> Movies { get; set; }
+
+        //public DbSet<MembershipType> MembershipTypes { get; set; }
+
+        //public DbSet<Genre> Genres { get; set; }
+
+        //public DbSet<Rental> Rentals { get; set; }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
