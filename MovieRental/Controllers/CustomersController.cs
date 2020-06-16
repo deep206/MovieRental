d96756c77f7e46331ff8b85c6ec7d12091e38223
deep206 <deeppatel.ce@gmail.com>
2020-06-16 11:08:39 -0400

    changes to customer controller to display membership type and seed db

--- MovieRental/Controllers/CustomersController.cs
+++ MovieRental/Controllers/CustomersController.cs
@@ -1,5 +1,5 @@
 ﻿using MovieRental.Models;
-using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -7,30 +7,33 @@ namespace MovieRental.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+        
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
 
             return View(customer);
-        }
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer { Id = 1, Name = "John Smith" },
-                new Customer { Id = 2, Name = "Mary Williams" }
-            };
         }
     }
 }
