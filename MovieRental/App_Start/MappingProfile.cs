32bb0840dde801a6e623f2cf83d1b22a825bf872
Deep Patel <deeppatel.ce@gmail.com>
2021-01-18 18:09:42 -0500

    Added api for movies, implementation for delete customer, and bootboxjs add

--- MovieRental/App_Start/MappingProfile.cs
+++ MovieRental/App_Start/MappingProfile.cs
@@ -2,14 +2,23 @@
 using MovieRental.Dtos;
 using MovieRental.Models;
 
-namespace MovieRental.App_Start
+namespace Movi.App_Start
 {
     public class MappingProfile : Profile
     {
         public MappingProfile()
         {
+            // Domain to Dto
             Mapper.CreateMap<Customer, CustomerDto>();
-            Mapper.CreateMap<CustomerDto, Customer>();
+            Mapper.CreateMap<Movie, MovieDto>();
+
+
+            // Dto to Domain
+            Mapper.CreateMap<CustomerDto, Customer>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+
+            Mapper.CreateMap<MovieDto, Movie>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
         }
     }
 }
